c36abda15ec281948a72ab1e26bc9b78d0b8cf05
smalllqiang <F11F10E8@outlook.com>
2026-04-30 20:21:02 +0800

    chore: config

---
+++ tools/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+raw_dir = Path("raw_imgs")
+output_dir = Path("temp", "output")
+config_path = Path("tools", "scale.json")
+
+description_file_path = Path("tools", "descriptions.json")
+status_code_font = Path("tools", "MapleMonoNormalNL-Medium.ttf")
+description_en_font = Path("tools", "MapleMonoNormalNL-Regular.ttf")
+description_zh_font = Path("tools", "MapleMonoNormalNL-CN-Regular.ttf")
